d61e2c3b3421d8f39c988c3f3ddf935933b8e55d
Adam Barton <adam.barton333@gmail.com>
2018-07-22 04:16:34 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,15 @@
 module Memorable
   module ClassMethods
 
-  def reset_all
-    self.all.clear
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
   end
 
-  def count
-    self.all.count
-  end
+  module InstanceMethods
 
 end
